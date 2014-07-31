500d56ef57476578effa3be51fa5d0228b53fa01
Vinicius da Silva <dsilvavinicius@github.com>
2014-07-31 15:15:27 -0300

    Morton code creation tests passing correctly.

--- src/header/model/MortonCode.h
+++ src/header/model/MortonCode.h
@@ -2,6 +2,8 @@
 #define MORTON_CODE_H
 
 #include <vector>
+#include <stdexcept>
+#include <iostream>
 
 using namespace std;
 
@@ -19,7 +21,7 @@ namespace model
 	 * 0 ... | 1 | 110 | 100 | 110.
 	 */
 	template <typename T>
-	class MortonCodeBase
+	class MortonCode
 	{
 	public:
 		/** Use this method to calculate code from position. */
@@ -28,62 +30,111 @@ namespace model
 		/** Use this method to inform the code. */
 		void build(const T& codeBits);
 		
-		T getBits();
+		T getBits() const;
 		
-		MortonCodeBase< T > traverseUp(const MortonCodeBase<T>& code) const;
-		vector< MortonCodeBase < T > > traverseDown(const MortonCodeBase<T>& code) const;
-	protected:
-		T spread3(T);
+		MortonCode< T > traverseUp(const MortonCode<T>& code) const;
+		vector< MortonCode < T > > traverseDown(const MortonCode<T>& code) const;
+		T spread3(T x);
 	private:
 		T m_bits;
 	};
 	
-	template <typename T>
-	class MortonCode : public MortonCodeBase<T> {};
-	
+	/** "Spreads" coordinate bits to build Morton code. Applied bit-wise operations are explained here:
+	 * http://stackoverflow.com/a/18528775/1042102 */
 	template <>
-	class MortonCode<unsigned int> : public MortonCodeBase<unsigned int>
-	{
-	public:
-		/** Builds 32 bits Morton code from coordinates and level. Octrees can reach 10 levels max. */
-		/*void build(const unsigned int& x, const unsigned int& y, const unsigned int& z,
-											 const unsigned int& level);*/
-	private:
-		/** "Spreads" coordinate bits to build Morton code. Applied bit-wise operations are explained here:
-		 * http://stackoverflow.com/a/18528775/1042102 */
-		unsigned int spread3(unsigned int x);
-	};
+	unsigned int MortonCode<unsigned int>::spread3(unsigned int x);
 	
+	/** "Spreads" coordinate bits to build Morton code. Applied bit-wise operations are explained here:
+	 * http://stackoverflow.com/a/18528775/1042102 */
 	template <>
-	class MortonCode<unsigned long> : public MortonCodeBase<unsigned long>
-	{
-	public:
-		/** Builds 64 bits Morton code from coordinates and level. Octrees can reach 21 levels max. */
-		/*void build(const unsigned long& x, const unsigned long& y, const unsigned long& z,
-											  const unsigned int& level);*/
-	private:
-		/** "Spreads" coordinate bits to build Morton code. Applied bit-wise operations are explained here:
-		 * http://stackoverflow.com/a/18528775/1042102 */
-		unsigned long spread3(unsigned long x);
-	};
+	unsigned long MortonCode<unsigned long>::spread3(unsigned long x);
 	
+	/** "Spreads" coordinate bits to build Morton code. Applied bit-wise operations are explained here:
+	 * http://stackoverflow.com/a/18528775/1042102 */
 	template <>
-	class MortonCode<unsigned long long> : public MortonCodeBase<unsigned long long>
-	{
-	public:
-		/** Builds 128 bits Morton code from coordinates and level. Octrees can reach 42 levels max. The compiler can
-		 * complain about unsigned long long size. In this case don't use this type. */
-		/*void build(const unsigned long long& x, const unsigned long long& y,
-				   const unsigned long long& z, const unsigned int& level);*/
-	private:
-		/** "Spreads" coordinate bits to build Morton code. Applied bit-wise operations are explained here:
-		 * http://stackoverflow.com/a/18528775/1042102 */
-		unsigned long long spread3(unsigned long long x);
-	};
+	unsigned long long MortonCode<unsigned long long>::spread3(unsigned long long x);
 	
 	using ShallowMortonCode = MortonCode< unsigned int >;
 	using MediumMortonCode = MortonCode< unsigned long >;
 	using DeepMortonCode = MortonCode< unsigned long long >;
+	
+	template <typename T>
+	void MortonCode<T>::build(const T& x, const T& y, const T& z, const unsigned int& level)
+	{
+		m_bits = spread3(x) | (spread3(y) << 1) | (spread3(z) << 2);
+		
+		T twoPowLevel = (T)1 << (level * 3);
+		
+		// Discards all bits in positions greater than level * 3.
+		m_bits &= (twoPowLevel) - 1;
+		// Put the 1 sufix.
+		m_bits |= twoPowLevel;
+	}
+	
+	template <typename T>
+	void MortonCode<T>::build(const T& codeBits) { m_bits = codeBits; }
+	
+	template <typename T>
+	T MortonCode<T>::getBits() const{ return m_bits; }
+	
+	template <typename T>
+	MortonCode<T> MortonCode<T>::traverseUp(const MortonCode<T>& code) const
+	{
+		T bits = code.getBits() >> 3;
+		MortonCode<T> parentMorton;
+		parentMorton.build(bits);
+		return parentMorton;
+	}
+	
+	template <typename T>
+	vector< MortonCode< T > >  MortonCode<T>::traverseDown(const MortonCode<T>& code) const
+	{
+		vector< MortonCode<T> > children(8);
+		
+		for (int i = 0; i < 8; ++i)
+		{
+			children[i] = MortonCode<T>((code << 3) | i);
+		}
+	}
+	
+	template <>
+	unsigned int MortonCode< unsigned int >::spread3(unsigned int x)
+	{
+		x &= 0x3ff;
+		x = (x | x << 16) & 0x30000ff;
+		x = (x | x << 8) & 0x300f00f;
+		x = (x | x << 4) & 0x30c30c3;
+		x = (x | x << 2) & 0x9249249;
+		
+		return x;
+	}
+	
+	template <>
+	unsigned long MortonCode< unsigned long >::spread3(unsigned long x)
+	{
+		x &= 0x1fffffL;
+		x = (x | x << 32) & 0x1f00000000ffffL;
+		x = (x | x << 16) & 0x1f0000ff0000ffL;
+		x = (x | x << 8) & 0x100f00f00f00f00fL;
+		x = (x | x << 4) & 0x10c30c30c30c30c3L;
+		x = (x | x << 2) & 0x1249249249249249L;
+		
+		return x;
+	}
+	
+	template <>
+	unsigned long long MortonCode< unsigned long long >::spread3(unsigned long long x)
+	{
+		x &= 0x3ffffffffffLL;
+		x = (x | x << 64) & 0x3ff0000000000000000ffffffffLL;
+		x = (x | x << 32) & 0x3ff00000000ffff00000000ffffLL;
+		x = (x | x << 16) & 0x30000ff0000ff0000ff0000ff0000ffLL;
+		x = (x | x << 8) & 0x300f00f00f00f00f00f00f00f00f00fLL;
+		x = (x | x << 4) & 0x30c30c30c30c30c30c30c30c30c30c3LL;
+		x = (x | x << 2) & 0x9249249249249249249249249249249LL;
+		
+		return x;
+	}
 }
 
 #endif
--- src/test/cpp/model/MortonCodeTest.cpp
+++ src/test/cpp/model/MortonCodeTest.cpp
@@ -16,22 +16,31 @@ namespace model
 			ShallowMortonCode shallowMorton;
 			
 			shallowMorton.build(7, 5, 0, 3);
-			ASSERT_EQ(shallowMorton.getBits(), 0x2CB); // 10 1100 1011
+			unsigned int ibits = shallowMorton.getBits();
+			ASSERT_EQ(ibits, 0x2CB) << "Expected 0x2CB, received " << hex << ibits; // 10 1100 1011
 			
 			shallowMorton.build(7, 5, 0, 10); // 100 0000 0000 0000 0000 0000 1100 1011
-			ASSERT_EQ(shallowMorton.getBits(), 0x400000CB);
+			ibits = shallowMorton.getBits();
+			ASSERT_EQ(ibits, 0x400000CB) << "Expected 0x400000CB, received " << hex << ibits;
+			
+			// Verifying if the unsigned long has 8 bytes.
+			ASSERT_EQ(sizeof(unsigned long), 8);
 			
 			MediumMortonCode mediumMorton;
 			
 			mediumMorton.build(5000, 6000, 7000, 5);
-			ASSERT_EQ(mediumMorton.getBits(), 0xDA00); // 1110 1010 0000 0000
+			unsigned long lbits = mediumMorton.getBits();
+			ASSERT_EQ(lbits, 0xEA00) << "Expected 0xEA00, received " << hex << lbits; // 1110 1010 0000 0000
 			
 			mediumMorton.build(5000, 6000, 7000, 13);
-			ASSERT_EQ(mediumMorton.getBits(), 0x78BF396A00); // 0111 1000 1011 1111 0011 1001 0110 1010 0000 0000
+			lbits = mediumMorton.getBits();
+			// 1111 1000 1011 1111 0011 1001 0110 1010 0000 0000
+			ASSERT_EQ(lbits, 0xF8BF396A00) << "Expected 0xF8BF396A00, received " << hex << lbits;
 			
 			mediumMorton.build(5000, 6000, 7000, 21);
+			lbits = mediumMorton.getBits();
 			// 1000 0000 0000 0000 0000 0000 0111 1000 1011 1111 0011 1001 0110 1010 0000 0000
-			ASSERT_EQ(mediumMorton.getBits(), 0x80000078BF396A00);
+			ASSERT_EQ(lbits, 0x80000078BF396A00)  << "Expected 0x80000078BF396A00, received " << hex << lbits;
 		}
 		
 		TEST_F(MortonCodeTest, Traversal) {
