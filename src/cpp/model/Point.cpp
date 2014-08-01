926bc0772387e05692a214b3b238982536bfe79d
Vinícius <dsilva.vinicius@gmail.com>
2014-08-01 13:45:59 -0300

    Resolved compilation problems related with templates.

--- src/cpp/model/Point.cpp
+++ src/cpp/model/Point.cpp
@@ -2,16 +2,4 @@
 
 namespace model
 {
-	template <typename Vec3>
-	Point<Vec3>::Point(const Vec3& color, const Vec3& pos)
-	{
-		m_color = make_shared<Vec3>(color);
-		m_pos = make_shared<Vec3>(pos);
-	}
-	
-	template <typename Vec3>
-	shared_ptr<Vec3> Point<Vec3>::getColor() { return m_color; }
-	
-	template <typename Vec3>
-	shared_ptr<Vec3> Point<Vec3>::getPos() { return m_pos; }
 }
--- src/header/model/Point.h
+++ src/header/model/Point.h
@@ -23,6 +23,19 @@ namespace model
 		shared_ptr< Vec3 > m_pos;
 	};
 	
+	template <typename Vec3>
+	Point<Vec3>::Point(const Vec3& color, const Vec3& pos)
+	{
+		m_color = make_shared<Vec3>(color);
+		m_pos = make_shared<Vec3>(pos);
+	}
+	
+	template <typename Vec3>
+	shared_ptr<Vec3> Point<Vec3>::getColor() { return m_color; }
+	
+	template <typename Vec3>
+	shared_ptr<Vec3> Point<Vec3>::getPos() { return m_pos; }
+	
 	/** Point smart pointer. */
 	template <typename Vec3>
 	using PointPtr = shared_ptr< Point< Vec3 > >;
