51b2db7c427af4521bd5b236e4759761a70e09fb
Vinícius <dsilva.vinicius@gmail.com>
2015-02-25 16:51:20 -0300

    Finished writting Frustum Code. Need to test it.

--- src/cpp/model/Frustum.cpp
+++ src/cpp/model/Frustum.cpp
@@ -2,48 +2,105 @@
 
 namespace model
 {
+	Frustum::Frustum( const Matrix4f& modelViewProjection )
+	{
+		
+		for( int i = 0; i < 6; ++i )
+		{
+			m_planes.push_back( new Plane() );
+		}
+		
+		extractPlanes( modelViewProjection, true );
+	}
+	
+	Frustum::~Frustum()
+	{
+		for( Plane* plane : m_planes )
+		{
+			delete plane;
+		}
+	}
+	
+	void Frustum::update( const Matrix4f& modelViewProjection )
+	{
+		extractPlanes( modelViewProjection, true );
+	}
+	
 	void Frustum::extractPlanes( const Matrix4f& modelViewProjection, const bool& normalize )
 	{
-		/*
 		// Left clipping plane
-		m_planes[ 0 ].a = modelViewProjection._41 + modelViewProjection._11;
-		m_planes[ 0 ].b = modelViewProjection._42 + modelViewProjection._12;
-		m_planes[ 0 ].c = modelViewProjection._43 + modelViewProjection._13;
-		m_planes[ 0 ].d = modelViewProjection._44 + modelViewProjection._14;
+		Vector4f& coeffs = m_planes[ 0 ]->coeffs();
+		coeffs[ 0 ] = modelViewProjection( 3, 0 ) + modelViewProjection( 0, 0 );
+		coeffs[ 1 ] = modelViewProjection( 3, 1 ) + modelViewProjection( 0, 1 );
+		coeffs[ 2 ] = modelViewProjection( 3, 2 ) + modelViewProjection( 0, 2 );
+		coeffs[ 3 ] = modelViewProjection( 3, 3 ) + modelViewProjection( 0, 3 );
 		// Right clipping plane
-		m_planes[ 1 ].a = modelViewProjection._41 - modelViewProjection._11;
-		m_planes[ 1 ].b = modelViewProjection._42 - modelViewProjection._12;
-		m_planes[ 1 ].c = modelViewProjection._43 - modelViewProjection._13;
-		m_planes[ 1 ].d = modelViewProjection._44 - modelViewProjection._14;
+		coeffs = m_planes[ 1 ]->coeffs();
+		coeffs[ 0 ] = modelViewProjection( 3, 0 ) - modelViewProjection( 0, 0 );
+		coeffs[ 1 ] = modelViewProjection( 3, 1 ) - modelViewProjection( 0, 1 );
+		coeffs[ 2 ] = modelViewProjection( 3, 2 ) - modelViewProjection( 0, 2 );
+		coeffs[ 3 ] = modelViewProjection( 3, 3 ) - modelViewProjection( 0, 3 );
 		// Top clipping plane
-		m_planes[ 2 ].a = modelViewProjection._41 - modelViewProjection._21;
-		m_planes[ 2 ].b = modelViewProjection._42 - modelViewProjection._22;
-		m_planes[ 2 ].c = modelViewProjection._43 - modelViewProjection._23;
-		m_planes[ 2 ].d = modelViewProjection._44 - modelViewProjection._24;
+		coeffs = m_planes[ 2 ]->coeffs();
+		coeffs[ 0 ] = modelViewProjection( 3, 0 ) - modelViewProjection( 1, 0 );
+		coeffs[ 1 ] = modelViewProjection( 3, 1 ) - modelViewProjection( 1, 1 );
+		coeffs[ 2 ] = modelViewProjection( 3, 2 ) - modelViewProjection( 1, 2 );
+		coeffs[ 3 ] = modelViewProjection( 3, 3 ) - modelViewProjection( 1, 3 );
 		// Bottom clipping plane
-		m_planes[ 3 ].a = modelViewProjection._41 + modelViewProjection._21;
-		m_planes[ 3 ].b = modelViewProjection._42 + modelViewProjection._22;
-		m_planes[ 3 ].c = modelViewProjection._43 + modelViewProjection._23;
-		m_planes[ 3 ].d = modelViewProjection._44 + modelViewProjection._24;
+		coeffs = m_planes[ 3 ]->coeffs();
+		coeffs[ 0 ] = modelViewProjection( 3, 0 ) + modelViewProjection( 1, 0 );
+		coeffs[ 1 ] = modelViewProjection( 3, 1 ) + modelViewProjection( 1, 1 );
+		coeffs[ 2 ] = modelViewProjection( 3, 2 ) + modelViewProjection( 1, 2 );
+		coeffs[ 3 ] = modelViewProjection( 3, 3 ) + modelViewProjection( 1, 3 );
 		// Near clipping plane
-		m_planes[ 4 ].a = modelViewProjection._41 + modelViewProjection._31;
-		m_planes[ 4 ].b = modelViewProjection._42 + modelViewProjection._32;
-		m_planes[ 4 ].c = modelViewProjection._43 + modelViewProjection._33;
-		m_planes[ 4 ].d = modelViewProjection._44 + modelViewProjection._34;
+		coeffs = m_planes[ 4 ]->coeffs();
+		coeffs[ 0 ] = modelViewProjection( 3, 0 ) + modelViewProjection( 2, 0 );
+		coeffs[ 1 ] = modelViewProjection( 3, 1 ) + modelViewProjection( 2, 1 );
+		coeffs[ 2 ] = modelViewProjection( 3, 2 ) + modelViewProjection( 2, 2 );
+		coeffs[ 3 ] = modelViewProjection( 3, 3 ) + modelViewProjection( 2, 3 );
 		// Far clipping plane
-		m_planes[ 5 ].a = modelViewProjection._41 - modelViewProjection._31;
-		m_planes[ 5 ].b = modelViewProjection._42 - modelViewProjection._32;
-		m_planes[ 5 ].c = modelViewProjection._43 - modelViewProjection._33;
-		m_planes[ 5 ].d = modelViewProjection._44 - modelViewProjection._34;
+		coeffs = m_planes[ 5 ]->coeffs();
+		coeffs[ 0 ] = modelViewProjection( 3, 0 ) - modelViewProjection( 2, 0 );
+		coeffs[ 1 ] = modelViewProjection( 3, 1 ) - modelViewProjection( 2, 1 );
+		coeffs[ 2 ] = modelViewProjection( 3, 2 ) - modelViewProjection( 2, 2 );
+		coeffs[ 3 ] = modelViewProjection( 3, 3 ) - modelViewProjection( 2, 3 );
+		
 		// Normalize the plane equations, if requested
 		if ( normalize == true )
 		{
-			NormalizePlane( m_planes[0] );
-			NormalizePlane( m_planes[1] );
-			NormalizePlane( m_planes[2] );
-			NormalizePlane( m_planes[3] );
-			NormalizePlane( m_planes[4] );
-			NormalizePlane( m_planes[5] );
-		}*/
+			m_planes[ 0 ]->normalize();
+			m_planes[ 1 ]->normalize();
+			m_planes[ 2 ]->normalize();
+			m_planes[ 3 ]->normalize();
+			m_planes[ 4 ]->normalize();
+			m_planes[ 5 ]->normalize();
+		}
+	}
+	
+	bool Frustum::isCullable( const Box& box )
+	{
+		Vector3f boxSizes = box.sizes();
+		Vector3f boxMin = box.min();
+		Vector3f boxMax = box.max();
+		
+		// Performs frustum plane dilatation, finds n only, since p is necessary just for detecting intersections points
+		// and verify if they are inside or outside the frustum.
+		for( int i = 0; i < 6; ++i )
+		{
+			Plane* plane = m_planes[ i ];
+			Vector3f normal = plane->normal();
+			float dilatation = abs( boxSizes.dot( normal ) );
+			float offset = plane->offset() + dilatation;
+			Plane dilatatedPlane( normal, offset );
+			
+			Vector3f n;
+			
+			n[ 0 ] = ( normal[ 0 ] < 0 ) ? boxMax[ 0 ] : boxMin[ 0 ];
+			n[ 1 ] = ( normal[ 1 ] < 0 ) ? boxMax[ 1 ] : boxMin[ 1 ];
+			n[ 2 ] = ( normal[ 2 ] < 0 ) ? boxMax[ 2 ] : boxMin[ 2 ];
+			
+			if( dilatatedPlane.signedDistance( n ) > 0 ) return true;
+		}
+		return false;
 	}
 }
--- src/header/model/Frustum.h
+++ src/header/model/Frustum.h
@@ -1,10 +1,12 @@
 #ifndef FRUSTUM_H
 #define FRUSTUM_H
 
+#include <vector>
 #include <glm/glm.hpp>
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
 
+using namespace std;
 using namespace glm;
 using namespace Eigen;
 
@@ -13,21 +15,30 @@ namespace model
 	/** Camera's frustum. Provide culling of axis-aligned boxes. */
 	class Frustum
 	{
+		using Plane = Hyperplane< float, 3 >;
+		using Box = AlignedBox< float, 3 >;
 	public:
 		Frustum( const Matrix4f& modelViewProjection );
+		~Frustum();
 		
 		/** Updates the frustrum after changing camera's intrinsic parameters ( fov, ratio, near or far plane ). */
 		void update( const Matrix4f& modelViewProjection );
-	
 		
+		/** Performs optimized view frustum culling as explained in paper Optimized View Frustum Culling Algorithms for
+		 * Bounding Boxes. Available in: @link http://www.cse.chalmers.se/~uffe/vfc_bbox.pdf. Differently from the algorithm
+		 * there, we don't distinguish intersection and outside cases.
+		 * @returns true if the box is outside the frustum, false otherwise. */
+		inline bool isCullable( const Box& box );
 		
 	private:
-		/** Extracts the 6 frustum planes from the model-view-projection matrix.
+		/** Algorithm for extraction of the 6 frustum planes from the model-view-projection matrix as explained in paper
+		 * Fast Extraction of Viewing Frustum Planes from the World-View-Projection Matrix. Available in
+		 * @link gamedevs.org/uploads/fast-extraction-viewing-frustum-planes-from-world-view-projection-matrix.pdf
 		 * @param normalize indicates that the final plane equation should be normalized. */
 		void extractPlanes( const Matrix4f& modelViewProjection, const bool& normalize );
 		
 		// Planes are defined
-		Hyperplane< float, 3 >* m_planes[ 6 ];
+		vector< Plane* > m_planes;
 	};
 }
 
