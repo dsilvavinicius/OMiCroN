5e2383a403e2b2f521fb72f30c2825edf357b560
Vinícius <dsilva.vinicius@gmail.com>
2015-03-01 23:26:17 -0300

    In progress: updating Frustum class to work in world coordinates instead of eye coordinates.

--- src/cpp/model/Frustum.cpp
+++ src/cpp/model/Frustum.cpp
@@ -3,7 +3,7 @@
 
 namespace model
 {
-	Frustum::Frustum( const Matrix4f& modelViewProjection )
+	Frustum::Frustum( const Matrix4f& modelView, const Matrix4f& projection )
 	{
 		
 		for( int i = 0; i < 6; ++i )
@@ -11,7 +11,7 @@ namespace model
 			m_planes.push_back( new Plane() );
 		}
 		
-		extractPlanes( modelViewProjection, true );
+		extractPlanes( modelView, projection, true );
 	}
 	
 	Frustum::~Frustum()
@@ -22,67 +22,72 @@ namespace model
 		}
 	}
 	
-	void Frustum::update( const Matrix4f& modelViewProjection )
+	void Frustum::update( const Matrix4f& modelView, const Matrix4f& projection )
 	{
-		extractPlanes( modelViewProjection, true );
+		extractPlanes( modelView, projection, true );
 	}
 	
-	void Frustum::extractPlanes( const Matrix4f& modelViewProjection, const bool& normalize )
+	void Frustum::extractPlanes( const Matrix4f& modelView, const Matrix4f& projection, const bool& normalize )
 	{
 		// It seems that the d plane coefficient has wrong sign in the original algorithm, so all d coefficients are being
 		// multiplied by -1 in this implementation.
 		
 		// Left clipping plane
 		Vector4f& leftCoeffs = m_planes[ 0 ]->coeffs();
-		leftCoeffs[ 0 ] = modelViewProjection( 3, 0 ) + modelViewProjection( 0, 0 );
-		leftCoeffs[ 1 ] = modelViewProjection( 3, 1 ) + modelViewProjection( 0, 1 );
-		leftCoeffs[ 2 ] = modelViewProjection( 3, 2 ) + modelViewProjection( 0, 2 );
-		leftCoeffs[ 3 ] = -( modelViewProjection( 3, 3 ) + modelViewProjection( 0, 3 ) );
+		leftCoeffs[ 0 ] = projection( 3, 0 ) + projection( 0, 0 );
+		leftCoeffs[ 1 ] = projection( 3, 1 ) + projection( 0, 1 );
+		leftCoeffs[ 2 ] = projection( 3, 2 ) + projection( 0, 2 );
+		leftCoeffs[ 3 ] = -( projection( 3, 3 ) + projection( 0, 3 ) );
 		
 		// Right clipping plane
 		Vector4f& rightCoeffs = m_planes[ 1 ]->coeffs();
-		rightCoeffs[ 0 ] = modelViewProjection( 3, 0 ) - modelViewProjection( 0, 0 );
-		rightCoeffs[ 1 ] = modelViewProjection( 3, 1 ) - modelViewProjection( 0, 1 );
-		rightCoeffs[ 2 ] = modelViewProjection( 3, 2 ) - modelViewProjection( 0, 2 );
-		rightCoeffs[ 3 ] = -( modelViewProjection( 3, 3 ) - modelViewProjection( 0, 3 ) );
+		rightCoeffs[ 0 ] = projection( 3, 0 ) - projection( 0, 0 );
+		rightCoeffs[ 1 ] = projection( 3, 1 ) - projection( 0, 1 );
+		rightCoeffs[ 2 ] = projection( 3, 2 ) - projection( 0, 2 );
+		rightCoeffs[ 3 ] = -( projection( 3, 3 ) - projection( 0, 3 ) );
 		
 		// Top clipping plane
 		Vector4f& topCoeffs = m_planes[ 2 ]->coeffs();
-		topCoeffs[ 0 ] = modelViewProjection( 3, 0 ) - modelViewProjection( 1, 0 );
-		topCoeffs[ 1 ] = modelViewProjection( 3, 1 ) - modelViewProjection( 1, 1 );
-		topCoeffs[ 2 ] = modelViewProjection( 3, 2 ) - modelViewProjection( 1, 2 );
-		topCoeffs[ 3 ] = -( modelViewProjection( 3, 3 ) - modelViewProjection( 1, 3 ) );
+		topCoeffs[ 0 ] = projection( 3, 0 ) - projection( 1, 0 );
+		topCoeffs[ 1 ] = projection( 3, 1 ) - projection( 1, 1 );
+		topCoeffs[ 2 ] = projection( 3, 2 ) - projection( 1, 2 );
+		topCoeffs[ 3 ] = -( projection( 3, 3 ) - projection( 1, 3 ) );
 		
 		// Bottom clipping plane
 		Vector4f& botCoeffs = m_planes[ 3 ]->coeffs();
-		botCoeffs[ 0 ] = modelViewProjection( 3, 0 ) + modelViewProjection( 1, 0 );
-		botCoeffs[ 1 ] = modelViewProjection( 3, 1 ) + modelViewProjection( 1, 1 );
-		botCoeffs[ 2 ] = modelViewProjection( 3, 2 ) + modelViewProjection( 1, 2 );
-		botCoeffs[ 3 ] = -( modelViewProjection( 3, 3 ) + modelViewProjection( 1, 3 ) );
+		botCoeffs[ 0 ] = projection( 3, 0 ) + projection( 1, 0 );
+		botCoeffs[ 1 ] = projection( 3, 1 ) + projection( 1, 1 );
+		botCoeffs[ 2 ] = projection( 3, 2 ) + projection( 1, 2 );
+		botCoeffs[ 3 ] = -( projection( 3, 3 ) + projection( 1, 3 ) );
 		
 		// Near clipping plane
 		Vector4f& nearCoeffs = m_planes[ 4 ]->coeffs();
-		nearCoeffs[ 0 ] = modelViewProjection( 3, 0 ) + modelViewProjection( 2, 0 );
-		nearCoeffs[ 1 ] = modelViewProjection( 3, 1 ) + modelViewProjection( 2, 1 );
-		nearCoeffs[ 2 ] = modelViewProjection( 3, 2 ) + modelViewProjection( 2, 2 );
-		nearCoeffs[ 3 ] = -( modelViewProjection( 3, 3 ) + modelViewProjection( 2, 3 ) );
+		nearCoeffs[ 0 ] = projection( 3, 0 ) + projection( 2, 0 );
+		nearCoeffs[ 1 ] = projection( 3, 1 ) + projection( 2, 1 );
+		nearCoeffs[ 2 ] = projection( 3, 2 ) + projection( 2, 2 );
+		nearCoeffs[ 3 ] = -( projection( 3, 3 ) + projection( 2, 3 ) );
 		
 		// Far clipping plane
 		Vector4f& farCoeffs = m_planes[ 5 ]->coeffs();
-		farCoeffs[ 0 ] = modelViewProjection( 3, 0 ) - modelViewProjection( 2, 0 );
-		farCoeffs[ 1 ] = modelViewProjection( 3, 1 ) - modelViewProjection( 2, 1 );
-		farCoeffs[ 2 ] = modelViewProjection( 3, 2 ) - modelViewProjection( 2, 2 );
-		farCoeffs[ 3 ] = -( modelViewProjection( 3, 3 ) - modelViewProjection( 2, 3 ) );
+		farCoeffs[ 0 ] = projection( 3, 0 ) - projection( 2, 0 );
+		farCoeffs[ 1 ] = projection( 3, 1 ) - projection( 2, 1 );
+		farCoeffs[ 2 ] = projection( 3, 2 ) - projection( 2, 2 );
+		farCoeffs[ 3 ] = -( projection( 3, 3 ) - projection( 2, 3 ) );
 		
-		// Normalize the plane equations, if requested
 		if ( normalize )
 		{
-			m_planes[ 0 ]->normalize();
-			m_planes[ 1 ]->normalize();
-			m_planes[ 2 ]->normalize();
-			m_planes[ 3 ]->normalize();
-			m_planes[ 4 ]->normalize();
-			m_planes[ 5 ]->normalize();
+			for( Plane* plane : m_planes )
+			{
+				plane->normalize();
+			}
+		}
+		
+		Matrix4f inverse = modelView.inverse();
+		Affine3f toWorld( inverse );
+		
+		for( Plane* plane : m_planes )
+		{
+			plane->transform( toWorld );
 		}
 		
 		//
--- src/header/model/Frustum.h
+++ src/header/model/Frustum.h
@@ -14,17 +14,18 @@ using namespace Eigen;
 
 namespace model
 {
-	/** Camera's frustum. Provide culling of axis-aligned boxes. */
+	/** Camera's frustum. Provide culling of axis-aligned boxes. The camera frustum planes will be in world coordinates.*/
 	class Frustum
 	{
 		using Plane = Hyperplane< float, 3 >;
 		using Box = AlignedBox< float, 3 >;
 	public:
-		Frustum( const Matrix4f& modelViewProjection );
+		
+		Frustum( const Matrix4f& modelView, const Matrix4f& projection );
 		~Frustum();
 		
 		/** Updates the frustrum after changing camera's intrinsic parameters ( fov, ratio, near or far plane ). */
-		void update( const Matrix4f& modelViewProjection );
+		void update( const Matrix4f& modelView, const Matrix4f& projection );
 		
 		/** Performs optimized view frustum culling as explained in paper Optimized View Frustum Culling Algorithms for
 		 * Bounding Boxes. Available in: @link http://www.cse.chalmers.se/~uffe/vfc_bbox.pdf. Differently from the algorithm
@@ -35,9 +36,9 @@ namespace model
 	private:
 		/** Algorithm for extraction of the 6 frustum planes from the model-view-projection matrix as explained in paper
 		 * Fast Extraction of Viewing Frustum Planes from the World-View-Projection Matrix. Available in
-		 * @link gamedevs.org/uploads/fast-extraction-viewing-frustum-planes-from-world-view-projection-matrix.pdf
+		 * @link gamedevs.org/uploads/fast-extraction-viewing-frustum-planes-from-world-view-projection-matrix.pdf .
 		 * @param normalize indicates that the final plane equation should be normalized. */
-		void extractPlanes( const Matrix4f& modelViewProjection, const bool& normalize );
+		void extractPlanes( const Matrix4f& modelView, const Matrix4f& projection, const bool& normalize );
 		
 		// Planes are defined
 		vector< Plane* > m_planes;
