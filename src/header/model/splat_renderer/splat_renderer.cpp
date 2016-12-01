c36c7475ab3f9ad7b83ec25b5f67909e223e02f9
Vinícius <dsilva.vinicius@gmail.com>
2016-12-01 14:47:44 -0200

    Changed base point cloud color to gray.

--- src/header/model/splat_renderer/splat_renderer.cpp
+++ src/header/model/splat_renderer/splat_renderer.cpp
@@ -85,7 +85,7 @@ SplatRenderer::SplatRenderer( Tucano::Camera* camera )
 	  m_soft_zbuffer(true), m_smooth(false),
       m_ewa_filter(false), m_multisample(false),
       m_pointsize_method(0), m_backface_culling(false),
-      m_color(Vector3f(0.0, 0.25f, 1.0f)), m_epsilon(5.0f * 1e-3f),
+      m_color(Vector3f(0.5f, 0.5f, 0.5f)), m_epsilon(5.0f * 1e-3f),
       m_shininess(8.0f), m_radius_scale(1.0f), m_ewa_radius(1.0f),
       m_renderedSplats( 0ul )
 {
